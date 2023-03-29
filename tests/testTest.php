90ed00aeef49e408e285f2d2c1b6424622b0ff6c
Rikke Blom <78023071+rikkeblom@users.noreply.github.com>
2023-03-29 21:50:31 +0200

    testTest

---
+++ tests/testTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Unit tests to see if the CI system works
+ * @author  Rikke Blom
+ * @version  1.0, March 2023
+ */
+
+
+use PHPUnit\Framework\TestCase;
+
+class TestTest extends TestCase {
+    public function testFake() {
+        $result = 2 + 2;
+        $expected = 4;
+        
+        $this->assertEquals($expected, $result);
+    }
+}
