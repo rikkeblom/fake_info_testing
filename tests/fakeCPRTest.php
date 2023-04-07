3349a899a89b8cc3ee297632ba714a76427f2660
Victoria <vigi2586@stud.kea.dk>
2023-04-07 09:39:35 +0200

    can now run test, did test, checked if array has rows, checked is address is an array

--- tests/fakeCPRTest.php
+++ tests/fakeCPRTest.php
@@ -4,7 +4,7 @@ require_once 'src/FakeInfo.php';
 
 use PHPUnit\Framework\TestCase;
 
-class Test_get_Cpr_Adress_Phone extends testCase {
+class Test_get_Cpr_Address_Phone extends testCase {
 
     private FakeInfo $fakeInfo;
     public function setUp(): void {
@@ -16,17 +16,18 @@ class Test_get_Cpr_Adress_Phone extends testCase {
     }
 
     // we want to test if the public function get fake person
-    // --TEST 1: Do we receive an associative array?
-    // --TEST 2: Check if array is empty
-    // --TEST 3: Check if the array contains the number of indexs we expected
-    // --TEST 4: Check if all the indexs are there 
-    // --TEST 5: Does the array contain the 7 keys (CPR, firstName, lastName, gender, birthDate, address, phoneNumber)?
+    // --TEST 1: Do we receive an associative array?                                -- DONE --
+    // --TEST 2: Check if array is empty                                            -- DONE --
+    // --TEST 3: Check if the array contains the rows "CPR ADDRESS PHONENUMBER"     -- DONE --
+    // --TEST 4: Check if address is an array                                       -- DONE --
+    // --TEST 5: Check if the array "address" has all its rows
+
 
 
     // TEST IF WE RECIVE AN ARRAY
     public function testIfArray() {
         $result = is_array($this->fakeInfo->getFakePerson());
-        $expected = 'true';
+        $expected = true;
 
         $this->assertEquals($result, $expected);
     }
@@ -36,20 +37,43 @@ class Test_get_Cpr_Adress_Phone extends testCase {
     // TEST IF ARRAY IS EMPTY
     public function testIfEmpty() {
         $result = empty($this->fakeInfo->getFakePerson());
-        $expected = 'false';
+        $expected = false;
 
         $this->assertEquals($result, $expected);
     }
 
 
 
-    // TEST IF WE RECIEVE AN ARRAY
-    public function test_if_array() {
-        $result = gettype($this->fakeInfo->getFakePerson());
-        $expected = 'array';
-        
+    /**
+     * @dataProvider arrayRows
+     */
+
+    // TEST IF THE ARRAY HAS THE ROWS "CPR ADDRESS PHONE_NUMBER"
+    public function test_if_array_has_rows($row, $expected) {
+
+        $result = array_key_exists($row, $this->fakeInfo->getFakePerson());
+
         $this->assertEquals($expected, $result);
     }
+
+    public function arrayRows() {
+        return [
+            ['CPR', true],  
+            ['address', true], 
+            ['phoneNumber', true],
+        ];
+    }
+
+
+
+    // TEST IF "address" IS AN ARRAY
+    public function test_if_address_is_array() {
+        $result = is_array($this->fakeInfo->getFakePerson('address'));
+        $expected = true;
+    
+        $this->assertEquals($result, $expected);
+    }
+
 
 
 }
