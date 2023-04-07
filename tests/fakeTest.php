a1f8f234f5db167822aabd8f4a2a3f9270ac0176
Frederik Kristensen <frede4947@email.com>
2023-04-07 04:09:31 +0200

    setup of tests

---
+++ tests/fakeTest.php
@@ -0,0 +1,117 @@
+<?php
+
+require_once 'src/Fakeinfo.php';
+
+
+
+use PHPUnit/Framework/TestCase;
+
+class TestgetCprFullNameAndGender extends testCase {
+
+    $testInfo = getCprFullNameAndGender();
+
+    // Test we wanna do is 
+    //  - Check if we get a array
+    //  - Check if array is empty
+    //  - Check if the array contains the number of indexs we expected
+    //  - Check if all the indexs are there 
+
+    public function testIfArray() {
+        $result = is_array($testInfo);
+        $expected = "true";
+
+        $this->assertEquals($result, $expected);
+    }
+
+    public function testIfEmpty() {
+        $result = empty($testInfo);
+        $expected = "false";
+
+        $this->assertEquals($result, $expected);
+    }
+
+    public function testArraySize() {
+        $result = sizeof($result);
+        $expected = 4;
+
+        $this->assertEquals($result, $expected);
+    }
+
+    public function testIndexs() {
+        $result = 0
+
+        if (in_array('CPR') = true) {
+            $result = $result + 1;
+        }
+        if (in_array('firstName') = true) {
+            $result = $result + 1;
+        }
+        if (in_array('lastName') = true) {
+            $result = $result + 1;
+        }
+        if (in_array('gender') = true) {
+            $result = $result + 1;
+        }
+
+        $expected = 4;
+
+        $this->assertEquals($result, $expected);
+    }
+
+}
+
+class TestgetCprFullNameGenderAndBirthDate extends testCase {
+
+    $testInfo = getCprFullNameGenderAndBirthDate();
+
+    // Test we wanna do is 
+    //  - Check if we get a array
+    //  - Check if array is empty   
+    //  - Check if the array contains the number of indexs we expected
+    //  - Check if all the indexs are there 
+
+    public function testIfArray() {
+        $result = is_array($testInfo);
+        $expected = "true";
+
+        $this->assertEquals($result, $expected);
+    }
+
+    public function testIfEmpty() {
+        $result = empty($testInfo);
+        $expected = "false";
+
+        $this->assertEquals($result, $expected);
+    }
+
+    public function testArraySize() {
+        $result = sizeof($result);
+        $expected = 5;
+
+        $this->assertEquals($result, $expected);
+    }
+
+    public function testIndexs() {
+        $result = 0
+
+        if (in_array('CPR') = true) {
+            $result = $result + 1;
+        }
+        if (in_array('firstName') = true) {
+            $result = $result + 1;
+        }
+        if (in_array('lastName') = true) {
+            $result = $result + 1;
+        }
+        if (in_array('gender') = true) {
+            $result = $result + 1;
+        }
+        if (in_array('birthDate') = true) {
+            $result = $result + 1;
+        }
+
+        $expected = 5;
+
+        $this->assertEquals($result, $expected);
+    }
+}
