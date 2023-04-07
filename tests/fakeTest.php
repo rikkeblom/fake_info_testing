8a43539664304f14beb8a1d8f163713fe1525b40
Frederik Kristensen <frede4947@email.com>
2023-04-07 10:24:21 +0200

    test

--- tests/fakeTest.php
+++ tests/fakeTest.php
@@ -16,7 +16,7 @@ class TestgetCprFullNameAndGender extends testCase {
         unset($this->fakeinfo);
     }
     
-    const $testInfo = getCprFullNameAndGender();
+    // const $testInfo = getCprFullNameAndGender();
 
     // Test we wanna do is 
     //  - Check if we get a array
@@ -31,95 +31,95 @@ class TestgetCprFullNameAndGender extends testCase {
         $this->assertEquals($result, $expected);
     }
 
-    public function testIfEmpty() {
-        $result = empty($testInfo);
-        $expected = "false";
+    // public function testIfEmpty() {
+    //     $result = empty($testInfo);
+    //     $expected = "false";
 
-        $this->assertEquals($result, $expected);
-    }
+    //     $this->assertEquals($result, $expected);
+    // }
 
-    public function testArraySize() {
-        $result = sizeof($result);
-        $expected = 4;
+    // public function testArraySize() {
+    //     $result = sizeof($result);
+    //     $expected = 4;
 
-        $this->assertEquals($result, $expected);
-    }
+    //     $this->assertEquals($result, $expected);
+    // }
 
-    public function testIndexs() {
-        $result = 0
+    // public function testIndexs() {
+    //     $result = 0
 
-        if (in_array('CPR') = true) {
-            $result = $result + 1;
-        }
-        if (in_array('firstName') = true) {
-            $result = $result + 1;
-        }
-        if (in_array('lastName') = true) {
-            $result = $result + 1;
-        }
-        if (in_array('gender') = true) {
-            $result = $result + 1;
-        }
+    //     if (in_array('CPR') = true) {
+    //         $result = $result + 1;
+    //     }
+    //     if (in_array('firstName') = true) {
+    //         $result = $result + 1;
+    //     }
+    //     if (in_array('lastName') = true) {
+    //         $result = $result + 1;
+    //     }
+    //     if (in_array('gender') = true) {
+    //         $result = $result + 1;
+    //     }
 
-        $expected = 4;
+    //     $expected = 4;
 
-        $this->assertEquals($result, $expected);
-    }
+    //     $this->assertEquals($result, $expected);
+    // }
 
 }
 
-class TestgetCprFullNameGenderAndBirthDate extends testCase {
+// class TestgetCprFullNameGenderAndBirthDate extends testCase {
 
-    $testInfo = getCprFullNameGenderAndBirthDate();
+//     $testInfo = getCprFullNameGenderAndBirthDate();
 
-    // Test we wanna do is 
-    //  - Check if we get a array
-    //  - Check if array is empty   
-    //  - Check if the array contains the number of indexs we expected
-    //  - Check if all the indexs are there 
+//     // Test we wanna do is 
+//     //  - Check if we get a array
+//     //  - Check if array is empty   
+//     //  - Check if the array contains the number of indexs we expected
+//     //  - Check if all the indexs are there 
 
-    public function testIfArray() {
-        $result = is_array($testInfo);
-        $expected = "true";
+//     public function testIfArray() {
+//         $result = is_array($testInfo);
+//         $expected = "true";
 
-        $this->assertEquals($result, $expected);
-    }
+//         $this->assertEquals($result, $expected);
+//     }
 
-    public function testIfEmpty() {
-        $result = empty($testInfo);
-        $expected = "false";
+//     public function testIfEmpty() {
+//         $result = empty($testInfo);
+//         $expected = "false";
 
-        $this->assertEquals($result, $expected);
-    }
+//         $this->assertEquals($result, $expected);
+//     }
 
-    public function testArraySize() {
-        $result = sizeof($result);
-        $expected = 5;
+//     public function testArraySize() {
+//         $result = sizeof($result);
+//         $expected = 5;
 
-        $this->assertEquals($result, $expected);
-    }
+//         $this->assertEquals($result, $expected);
+//     }
 
-    public function testIndexs() {
-        $result = 0
+//     public function testIndexs() {
+//         $result = 0
 
-        if (in_array('CPR') = true) {
-            $result = $result + 1;
-        }
-        if (in_array('firstName') = true) {
-            $result = $result + 1;
-        }
-        if (in_array('lastName') = true) {
-            $result = $result + 1;
-        }
-        if (in_array('gender') = true) {
-            $result = $result + 1;
-        }
-        if (in_array('birthDate') = true) {
-            $result = $result + 1;
-        }
+//         if (in_array('CPR') = true) {
+//             $result = $result + 1;
+//         }
+//         if (in_array('firstName') = true) {
+//             $result = $result + 1;
+//         }
+//         if (in_array('lastName') = true) {
+//             $result = $result + 1;
+//         }
+//         if (in_array('gender') = true) {
+//             $result = $result + 1;
+//         }
+//         if (in_array('birthDate') = true) {
+//             $result = $result + 1;
+//         }
 
-        $expected = 5;
+//         $expected = 5;
 
-        $this->assertEquals($result, $expected);
-    }
-}
+//         $this->assertEquals($result, $expected);
+//     }
+// }
