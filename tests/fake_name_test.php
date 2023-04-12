fbd5889c29cf249daa99f53cbddf9b48eff6764f
Lukas Ross <sakul10000@gmail.com>
2023-04-12 13:00:48 +0200

    nye test birth

--- tests/fake_name_test.php
+++ tests/fake_name_test.php
@@ -28,4 +28,42 @@ class Fake_Full_Name_Test extends TestCase {
     }
 
 
+    public function testIfEmpty() {
+        $result = empty($this->fakeInfo->getFullNameGender());
+
+        $this->assertFalse($result);
+    }
+
+    public function testArraySize() {
+        $result = sizeof($this->fakeInfo->getFullNameGender());
+        $expected = 5;
+
+        $this->assertEquals($result, $expected);
+    }
+
+    // Testing if the keys we want are there
+
+    /**
+     * @dataProvider provideFullNameAndGender
+     */
+
+    public function testkeyFullNameAndGender($key) {
+        $result = array_key_exists($key, $this->fakeInfo->getFullNameGender());
+        $this->assertTrue($result);
+    }
+
+    public function provideFullNameAndGender() {
+        return [
+            ['firstName'],
+            ['lastName'],
+            ['gender']
+            
+        ];
+    }
+    
+
+
+
+
+
 }
